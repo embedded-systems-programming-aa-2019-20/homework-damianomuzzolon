ea607ffcfe01fcb3a20aa2430aa2396a8290a6d7
Damiano Muzzolon <damiano.muzzolon@studenti.unipd.it>
2019-12-20 22:10:05 +0100

    Caricato monitor per continuare domani con altro computer. Manca test dei costruttori

---
+++ File_miei/Park1.cpp
@@ -0,0 +1,15 @@
+//Park1.cpp
+
+#include "Park1.h"
+#include "Auto.h"
+#include "Date.h"
+#include <mutex>
+using std::mutex;
+using std::unique_lock;
+#include <cstdbool>
+#include <list>
+using std::list;
+
+//costruttori
+Park1::Park1(int cap):capienza{cap},prossima_entrata{1900, Date::Month::jan,1 ,0 ,0},prossima_uscita{1900, Date::Month::jan,1 ,0 ,0}, is_entrata_aggiornata{false}, is_uscita_aggiornata{false}, auto_presenti{cap} {}
+Park1::Park1():capienza{0},prossima_entrata{1900, Date::Month::jan,1 ,0 ,0},prossima_uscita{1900, Date::Month::jan,1 ,0 ,0}, is_entrata_aggiornata{false}, is_uscita_aggiornata{false}, auto_presenti{1} {}
---
+++ File_miei/Park1.h
@@ -0,0 +1,37 @@
+//Park1.h
+
+#ifndef PARK1_H
+#define PARK1_H
+
+#include <mutex>
+using std::mutex;
+#include <list>
+using std::list;
+#include <cstdbool>
+#include "Auto.h"
+#include "Date.h"
+
+class Park1{
+	public:
+		//costruttori
+		Park1();
+		Park1(int cap);
+		//getter
+		Date get_prossima_entrata() const {return prossima_entrata;}
+		Date get_prossima_uscita() const {return prossima_uscita;}
+		list<Auto> get_auto_presenti() const {return auto_presenti;}
+		bool get_is_uscita_aggiornata() const {return is_uscita_aggiornata;}
+		bool get_is_entrata_aggiornata() const {return is_entrata_aggiornata;}
+	private:
+		list<Auto> auto_presenti;
+		int capienza;
+		Date prossima_entrata;
+		Date prossima_uscita;
+
+		bool is_entrata_aggiornata;
+		bool is_uscita_aggiornata;
+
+		mutex mutex_;
+};
+
+#endif
